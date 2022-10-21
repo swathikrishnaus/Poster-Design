86e37b89439f504dc8910d5ab506d03fc50fc755
swathikrishnaus <swathikrishnaus2015@gmail.com>
2022-10-21 19:00:42 +0530

    updated

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// -------------------------Navbar shadow start--------------------------------
+
+$(document).scroll(function () {
+    var scroll = $(this).scrollTop();
+    if (scroll > 50)
+        $(".navbar").addClass("nav-shadow")
+    else
+        $(".navbar").removeClass("nav-shadow")
+})
+
+// -------------------------Navbar shadow end--------------------------------
+
+
+
+
+
+// -------------------------toggle close end--------------------------------
+
+
+
+// -------------------------toggle close end--------------------------------
